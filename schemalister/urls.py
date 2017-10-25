12303776717731aded8436c1827b4fcb0bb0db9b
MTabishKhan <33030661+MTabishKhan@users.noreply.github.com>
2017-10-25 13:20:15 +0530

    Add files via upload

---
+++ schemalister/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView, RedirectView
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'getschema.views.index', name='index'),
+    url(r'^oauth_response/$', 'getschema.views.oauth_response', name='oauth_response'),
+    url(r'^schema/(?P<schema_id>[0-9A-Za-z_\-]+)/$', 'getschema.views.view_schema', name='view_schema'),
+    url(r'^export/(?P<schema_id>[0-9A-Za-z_\-]+)/$', 'getschema.views.export', name='export'),
+    url(r'^logout/$', 'getschema.views.logout', name='logout'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^job_status/(?P<schema_id>[0-9A-Za-z_\-]+)/$', 'getschema.views.job_status'),
+    url(r'^loading/(?P<schema_id>[0-9A-Za-z_\-]+)/$', 'getschema.views.loading'),
+    url(r'^delete_schema/(?P<schema_id>[0-9A-Za-z_\-]+)/$', 'getschema.views.delete_schema', name='delete_schema'),
+    url(r'^auth_details/$', 'getschema.views.auth_details'),
+)
